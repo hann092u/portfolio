49bfc3d462476c150b66d9ab0e8265039c416b59
Hanna Israielian <hann092u@stud.kea.dk>
2023-01-01 21:03:57 +0100

    menu

--- portfolio/script.js
+++ portfolio/script.js
@@ -3,26 +3,24 @@ function burgerMenu(selector) {
   let body = document.querySelector("body");
   let button = menu.querySelector(".brg_menu_button");
   let overlay = menu.querySelector(".brg_menu_overlay");
-  let links= menu.querySelectorAll('.brg_menu_link')
+  let links = menu.querySelectorAll(".brg_menu_link");
 
-  button.addEventListener("click", e => {
+  button.addEventListener("click", (e) => {
     e.preventDefault();
     toggleMenu();
   });
 
-  links.forEach(link => {
-    link.addEventListener('click',()=>
-    toggleMenu());
-overlay.addEventListener("click", () => toggleMenu());
+  links.forEach((link) => link.addEventListener("click", () => toggleMenu()));
+  overlay.addEventListener("click", () => toggleMenu());
 
   function toggleMenu() {
-    if (menu.classList("brg_menu_active")) {
-     menu.classList.remove('brg_menu_active');
-     body.general.overflow='visible';
+    if (menu.classList.contains("brg_menu_active")) {
+      menu.classList.remove("brg_menu_active");
+      body.style.overflow = "visible";
     } else {
-        menu.classList.add('brg_menu_active');
-      body.general.overflow= "hidden";
+      menu.classList.add("brg_menu_active");
+      body.style.overflow = "hidden";
     }
   }
 }
-burgerMenu('.brg_menu');
+burgerMenu(".brg_menu");
