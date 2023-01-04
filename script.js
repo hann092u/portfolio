4d5a73356d195af46f2b997a90895c7964288342
Hanna Israielian <hann092u@stud.kea.dk>
2023-01-04 16:59:55 +0100

    5

--- script.js
+++ script.js
@@ -28,19 +28,31 @@ burgerMenu(".brg_menu");
 
 /******************* Slide-show ****************/
 let slideIndex = 1;
+let slideIndex2 = 1;
 showSlides(slideIndex);
+showSlides2(slideIndex2);
 
 // Next/previous controls
 function plusSlides(n) {
+  console.log("plusSlides");
   showSlides((slideIndex += n));
 }
-
+function plusSlides2(n) {
+  console.log("plusSlides2");
+  showSlides2((slideIndex2 += n));
+}
 // Thumbnail image controls
 function currentSlide(n) {
+  console.log("currentSlide");
   showSlides((slideIndex = n));
+}
+function currentSlide2(n) {
+  console.log("currentSlide2");
+  showSlides2((slideIndex2 = n));
 }
 
 function showSlides(n) {
+  console.log("showSlides");
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
@@ -58,4 +70,25 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+}
+
+function showSlides2(n) {
+  console.log("showSlides2");
+  let e;
+  let slides2 = document.getElementsByClassName("mySlides2");
+  let dots2 = document.getElementsByClassName("dot2");
+  if (n > slides2.length) {
+    slideIndex2 = 1;
+  }
+  if (n < 1) {
+    slideIndex2 = slides2.length;
+  }
+  for (e = 0; e < slides2.length; e++) {
+    slides2[e].style.display = "none";
+  }
+  for (e = 0; e < dots2.length; e++) {
+    dots2[e].className = dots2[e].className.replace(" active", "");
+  }
+  slides2[slideIndex2 - 1].style.display = "block";
+  dots2[slideIndex2 - 1].className += " active";
 }
